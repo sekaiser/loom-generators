553c2bfd85b23f10fdacf7602a3ef4f3311c0f58
Sebastian Kaiser <sk4is3r@gmail.com>
2023-07-29 22:10:55 +0200

    Update Generators.java
    Remove commented code

--- lib/src/main/java/loom/generators/Generators.java
+++ lib/src/main/java/loom/generators/Generators.java
@@ -25,45 +25,6 @@ public class Generators {
         return GeneratorBuilders.newKelemenGenerator(forEachable);
     }
 
-//    public static <T> GeneratorService<T> newVirtualThreadGenerator(Consumer<Yieldable<T>> task) {
-//        return createNewVirtualThreadGenerator(() -> task.accept(RunnableGenerator::yield));
-//    }
-
-    //    public static <T> GeneratorService<T> newVirtualThreadGenerator(Runnable task) {
-//        return createNewVirtualThreadGenerator(task);
-//    }
-//
-//    private static <T> GeneratorService<T> createNewVirtualThreadGenerator(Runnable task) {
-//        ThreadFactory factory = newVirtualBuilder(Runnable::run).factory();
-//        return newVirtualThreadGenerator(factory, task);
-//    }
-//
-//    public static <T> GeneratorService<T> newVirtualThreadGenerator(
-//            ThreadFactory threadFactory,
-//            Runnable runnable
-//    ) {
-//        Thread t = threadFactory.newThread(runnable);
-//        return new RunnableGeneratorImpl<>(t::start);
-//    }
-//
-//    /**
-//     * Creates a virtual thread builder which runs continuations on given executor.
-//     *
-//     * @param executor runs continuations.
-//     * @return the virtual thread builder
-//     */
-//    private static Thread.Builder.OfVirtual newVirtualBuilder(Executor executor) {
-//        Objects.requireNonNull(executor);
-//        try {
-//            final Class<?> vtbclass = Class.forName("java.lang.ThreadBuilders$VirtualThreadBuilder");
-//            final Constructor<?> c = vtbclass.getDeclaredConstructor(Executor.class);
-//            c.setAccessible(true);
-//            return (Thread.Builder.OfVirtual) c.newInstance(executor);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//    }
-//
     @FunctionalInterface
     public interface Yieldable<T> {
         void yield(T t);
